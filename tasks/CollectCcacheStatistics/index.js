4ca294fab1709652273051c6a5b934176dc14411
Vargha Csongor Csaba (XC/ENG3-BP) <vac1bp@bosch.com>
2024-10-24 16:31:21 +0200

    add tags

--- tasks/CollectCcacheStatistics/index.js
+++ tasks/CollectCcacheStatistics/index.js
@@ -2,57 +2,6 @@ const fs = require('fs');
 const tl = require('azure-pipelines-task-lib/task');
 const path = require('path');
 
-async function run() {
-    try {
-        const ccacheLogFile = tl.getPathInput('ccacheLogFile', true, true);
-        const tempDir = tl.getVariable('Agent.TempDirectory');
-        const statsFile = path.join(tempDir, 'ccache_stats.json');
-
-        // Read the ccache log file
-        const ccacheLog = fs.readFileSync(ccacheLogFile, 'utf8');
-        const lines = ccacheLog.split('\n');
-        console.log('Ccache log file read successfully.');
-
-        // Parse the log for statistics
-        const ccacheStats = getCcacheStatisticFromLog(lines);
-
-        if (Object.keys(ccacheStats).length === 0) {
-            console.log('No ccache statistics found in the log.');
-            tl.setResult(tl.TaskResult.Failed, 'No ccache statistics found.');
-            return;
-        }
-
-        
-        let os;
-        try {
-            os = tl.getPlatform();
-        } catch {
-            os = 'Unknown';
-        }
-
-        // Get Azure DevOps environment variables for build information
-        const buildInfo = {
-            ccache_version: ccacheStats.build_info.ccache_version,
-            agent_name: tl.getVariable('Agent.Name'),
-            build_name: tl.getVariable('Build.DefinitionName'),
-            build_number: tl.getVariable('Build.BuildNumber'),
-            stage_name: tl.getVariable('System.StageName'),
-            platform: os
-        };
-
-        ccacheStats.build_info = buildInfo;
-
-        // Write statistics to a file
-        fs.writeFileSync(statsFile, JSON.stringify(ccacheStats));
-        console.log(`Ccache statistics written to ${statsFile}`);
-
-        // Set output variable for the next task
-        tl.setVariable('ccacheStatsFile', statsFile);
-    } catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
-    }
-}
-
 function getCcacheStatisticFromLog(fileLines) {
     let version = '';
     let versionMajor = 0;
@@ -106,4 +55,54 @@ function getCcacheStatisticFromLog(fileLines) {
     };
 }
 
-run();
+try {
+    const ccacheLogFile = tl.getPathInput('ccacheLogFile', true, true);
+    const tempDir = tl.getVariable('Agent.TempDirectory');
+    const statsFile = path.join(tempDir, 'ccache_stats.json');
+
+    // Read the ccache log file
+    const ccacheLog = fs.readFileSync(ccacheLogFile, 'utf8');
+    const lines = ccacheLog.split('\n');
+    console.log('Ccache log file read successfully.');
+
+    // Parse the log for statistics
+    const ccacheStats = getCcacheStatisticFromLog(lines);
+
+    if (Object.keys(ccacheStats).length === 0) {
+        console.log('No ccache statistics found in the log.');
+        tl.setResult(tl.TaskResult.Failed, 'No ccache statistics found.');
+        return;
+    }
+
+    
+    let os;
+    try {
+        os = tl.getPlatform();
+    } catch {
+        os = 'Unknown';
+    }
+
+    // Get Azure DevOps environment variables for build information
+    const buildInfo = {
+        ccache_version: ccacheStats.build_info.ccache_version,
+        agent_name: tl.getVariable('Agent.Name'),
+        build_name: tl.getVariable('Build.DefinitionName'),
+        build_number: tl.getVariable('Build.BuildNumber'),
+        stage_name: tl.getVariable('System.StageName'),
+        platform: os,
+        tags: {
+            type : tl.getInput('typeTag'),
+        }
+    };
+
+    ccacheStats.build_info = buildInfo;
+
+    // Write statistics to a file
+    fs.writeFileSync(statsFile, JSON.stringify(ccacheStats));
+    console.log(`Ccache statistics written to ${statsFile}`);
+
+    // Set output variable for the next task
+    tl.setVariable('ccacheStatsFile', statsFile);
+} catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err.message);
+}
